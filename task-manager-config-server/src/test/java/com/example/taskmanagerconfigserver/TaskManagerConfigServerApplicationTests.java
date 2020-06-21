2a78ff78aeb7fb4e10798c935634c9d13613d33f
abhi1377 <61047600+abhi1377@users.noreply.github.com>
2020-06-22 00:22:29 +0530

    Add files via upload

---
+++ task-manager-config-server/src/test/java/com/example/taskmanagerconfigserver/TaskManagerConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
